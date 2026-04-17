cef4438ae210f08b76b1c162e340c9a9161f7f8c
BrundevCoder <brunodevdrums@gmail.com>
2026-04-17 19:40:08 +0100

    Enhance excuse generation with history tracking
    Added functionality to track already displayed excuses and prevent duplicates. Introduced random color selection for displayed excuses.

--- script.js
+++ script.js
@@ -3,20 +3,43 @@ const excuseDisplay = document.getElementById("excuseDisplay");
 const generateButton = document.getElementById("generateBtn");
 const historicList = document.getElementById("historic-list");
 
+let alreadyDisplayedExcueses = [];
+
 const excuses = [
-  "Sorry, my dog eat me homework!",
+  "Sorry, my dog ate my homework!",
   "A time traveler told me that if I finished this homework, the universe would collapse.",
   "A glitch in the Matrix deleted my files and replaced them with cat memes.",
   "My code was so spicy that it set my local server on fire.",
   "My dog didn't eat my homework; he actually debugged it and found 42 errors.",
   "I was studying, but the letters on the page started a dance battle.",
-  "My Dog eat My code..."
+  "My Dog eat My code...",
+  "Lesson not found 404",
+  "Why am I late? Simple, I had to cross mountains and lakes just to get here.",
+  "I'm sleepy, I stayed up until 3 AM trying to find the bug on line 1474 (the line was empty).",
+  "I fell while my horse was crossing the road...",
+  "I've been trying to understand why '1' + '1' = '11'"
   // add more
 ];
 
 let historyExcuses = [];
 
 // functions
+
+function randomColor() {
+  let color = "";
+
+  // choose a number between 1 and 2
+  let randomNumber = Math.floor(Math.random() * 2);
+
+  if (randomNumber === 1) {
+    color = "red";
+  }
+  else {
+    color = "blue";
+  }
+
+  return color;
+}
 
 function getRandomExcuse() {
   let maxIndex = excuses.length;
@@ -34,8 +57,17 @@ function placeInHistory(excuse) {
   }
   historyExcuses.push(excuse);
 
+  let excuseColor = randomColor();
+
   const listItem = document.createElement("li");
   const node = document.createTextNode(`"${excuse}"`);
+
+  if (excuseColor === "red") {
+    listItem.classList.add("red-color");
+  }
+  else {
+    listItem.classList.add("blue-color");
+  }
 
   listItem.appendChild(node);
 
@@ -45,7 +77,17 @@ function placeInHistory(excuse) {
 function displayExcuse() {
   let excuse = getRandomExcuse();
 
+  if (excuses.length === alreadyDisplayedExcueses.length) {
+    excuseDisplay.innerText = `No more excuses! We only have ${excuses.length} excuses!`;
+    return;
+  }
+
+  while (alreadyDisplayedExcueses.includes(excuse)) {
+    excuse = getRandomExcuse();
+  }
+
   placeInHistory(excuse);
+  alreadyDisplayedExcueses.push(excuse);
 
   excuseDisplay.innerText = `${excuse}`;
 }
